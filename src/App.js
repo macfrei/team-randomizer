0e18bfd9239173d0224a882429dbbccaf79abe0c
macfrei <lene@7freiheiten.de>
2021-01-14 14:23:35 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import { useState } from 'react'
+import Form from './Form'
+import saveToLocal from './services/saveToLocal'
+import loadFromLocal from './services/loadFromLocal'
+import Teams from './Teams'
+
 function App() {
-  return <div>Hello World.</div>
+  const [data, setData] = useState(loadFromLocal('groups') || '')
+
+  return (
+    <>
+      <h1>Team {data.seed}</h1>
+      <Teams data={data} />
+      <Form onSubmit={submitForm} />
+    </>
+  )
+
+  function submitForm(formData) {
+    saveToLocal('groups', formData)
+    setData(formData)
+  }
 }
 
 export default App
