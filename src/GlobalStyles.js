19c9f527ab330b6e02741e73d3397c6dafd29a17
macfrei <lene@7freiheiten.de>
2021-01-14 14:28:18 +0100

    Install styled-components, add globalstyles

---
+++ src/GlobalStyles.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components/macro'
+
+export default createGlobalStyle`
+* {
+    box-sizing: border-box;
+}
+body {
+    display: grid;
+    font-family: sans-serif;
+    grid-template-rows: auto auto 1fr auto;
+    height: 100vh;
+    padding: 24px;
+}`
