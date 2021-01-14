ad37ef6e7cb5984faf084227c7336dc956bcfd0c
macfrei <lene@7freiheiten.de>
2021-01-14 15:46:37 +0100

    Rename Form and add styling

--- src/AppForm.js
+++ src/AppForm.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
+import styled from 'styled-components/macro'
 
-export default function Form({ onSubmit }) {
+export default function AppForm({ onSubmit }) {
   const [formData, setFormData] = useState({
     size: '' || 4,
     seed: '' || 'Web Development',
@@ -10,8 +11,8 @@ export default function Form({ onSubmit }) {
 
   return (
     <details>
-      <summary>Settings</summary>
-      <form onSubmit={handleSubmit}>
+      <Summary>Settings</Summary>
+      <Form onSubmit={handleSubmit}>
         <label>
           Team size:
           <input
@@ -49,7 +50,7 @@ export default function Form({ onSubmit }) {
           />
         </label>
         <button>save</button>
-      </form>
+      </Form>
     </details>
   )
 
@@ -65,3 +66,38 @@ export default function Form({ onSubmit }) {
     })
   }
 }
+
+const Form = styled.form`
+  display: grid;
+  grid-gap: 12px;
+  margin-top: 5px;
+
+  button {
+    justify-self: start;
+    background-color: #ff5a36;
+    border: 1px solid #ffded7;
+    color: white;
+    border-radius: 5px;
+    font-size: 100%;
+    padding: 2px 6px;
+    line-height: 1.5;
+  }
+
+  label {
+    display: block;
+  }
+
+  input {
+    display: block;
+    width: 100%;
+  }
+`
+const Summary = styled.summary`
+  cursor: default;
+  display: inline-block;
+  border: 1px solid #aaa5;
+  background: #eee;
+  padding: 2px 6px;
+  border-radius: 4px;
+  margin-bottom: 12px;
+`
