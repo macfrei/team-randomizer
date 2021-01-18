e6f657100b1af1bf5e19f53e9fcbf826ce4e31b2
macfrei <lene@7freiheiten.de>
2021-01-18 16:57:14 +0100

    Add some css variables

--- src/GlobalStyles.js
+++ src/GlobalStyles.js
@@ -16,4 +16,22 @@ export default createGlobalStyle`
         grid-column: 2;
     }
 }
+
+:root {
+    --blue-main: #193251;
+    --blue-75: #7589A2;
+    --blue-50: #E0E4E8;
+    --blue-25: #F8F8F8;
+
+    --orange-main: #FF5A36;
+    --orange-75: #FF9C86;
+    --orange-50: #FFBDAF;
+    --orange-25: #FFDED7;
+
+    --shadow-blue: 0px 2px 11px 0px rgba(25, 50, 81, 0.2);
+    --shadow-orange: 0px 4px 10px #ff5a3666;
+
+    --border-blue: 1px solid var(--blue-50);
+
+}
 `
